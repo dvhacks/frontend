ece5daf0ff07255e438614fd61228fe260448467
Matt Finucane <matfin@gmail.com>
2018-04-14 08:05:34 -0700

    feat(Dashboard): basic styling and placement of UI elements

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,56 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import FlatButton from 'material-ui/FlatButton';
+import {
+  AppBar,
+  BottomNavigation,
+  BottomNavigationItem,
+  FlatButton,
+  FontIcon,
+  List,
+  ListItem,
+  Paper
+} from 'material-ui';
 import { injectIntl, intlShape } from 'react-intl';
-import { GitHubIcon } from 'rmw-shell/lib/components/Icons';
-import { Activity } from 'rmw-shell';
-import muiThemeable from 'material-ui/styles/muiThemeable';
-import { withFirebase } from 'firekit-provider';
-import {FontIcon} from "material-ui";
+// import { Activity } from 'rmw-shell';
+// import { withFirebase } from 'firekit-provider';
 
-const currentYear = new Date().getFullYear();
-const daysPath = `/user_registrations_per_day/${currentYear}/${new Date().toISOString().slice(5, 7)}`;
-const monthsPath = `/user_registrations_per_month/${currentYear}`;
-const providerPath = `/provider_count`;
+const buttonOptions = {
+  backgroundColor: '#00cea5',
+  fullWidth: true,
+  hoverColor: '#00d9ae',
+  style: { color: '#fff' },
+  primary: true
+};
 
 class Dashboard extends Component {
-  componentDidMount () {
-    const { watchPath } = this.props;
-
-    watchPath(daysPath);
-    watchPath(monthsPath);
-    watchPath(providerPath);
-    watchPath('users_count');
-  }
 
   render () {
-    const { muiTheme, intl, days, months, providers, usersCount, history } = this.props;
-
     return (
-      <Activity
-        iconElementRight={
-          <FlatButton
-            style={{ marginTop: 4 }}
-            href="https://github.com/TarikHuber/react-most-wanted"
-            target="_blank"
-            rel="noopener"
-            secondary
-            icon={<GitHubIcon />}
-          />
-        }
-        title={intl.formatMessage({ id: 'dashboard' })} >
-
-        <FlatButton
-          onClick={() => {
-            history.push('/shipment/create')
-          }}
-          primary
-        >
-          <FontIcon style={{marginTop: 10}} className="material-icons" >add</FontIcon>
-          Send shipment
-        </FlatButton>
-
-      </Activity >
+      <Fragment>
+        <AppBar title="Grasshoppr" showMenuIconButton={false} />
+        <List>
+          <ListItem>
+            <FlatButton {...buttonOptions}>
+              Seller
+            </FlatButton>
+          </ListItem>
+          <ListItem>
+            <FlatButton {...buttonOptions}>
+              Buyer
+            </FlatButton>
+          </ListItem>
+          <ListItem>
+            <FlatButton {...buttonOptions}>
+              Courier
+            </FlatButton>
+          </ListItem>
+        </List>
+        <Paper zDepth={1}>
+          <BottomNavigation>
+            <BottomNavigationItem
+              icon={<FontIcon className="material-icons md-36">face</FontIcon>}
+            />
+          </BottomNavigation>
+        </Paper>
+      </Fragment>
     );
   }
 }
@@ -60,16 +62,9 @@ Dashboard.propTypes = {
 };
 
 const mapStateToProps = (state) => {
-  const { paths } = state;
-
-  return {
-    days: paths[daysPath],
-    months: paths[monthsPath],
-    providers: paths[providerPath],
-    usersCount: paths.users_count ? paths.users_count : 0
-  };
+  return {};
 };
 
 export default connect(
   mapStateToProps
-)(injectIntl(muiThemeable()(withFirebase(Dashboard))));
+)(injectIntl(Dashboard));
