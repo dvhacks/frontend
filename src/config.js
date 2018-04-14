695eb7d4c8bd9591c5638974ad1e901da00a9681
Matt Finucane <matfin@gmail.com>
2018-04-13 21:22:42 -0700

    chore: update firebase config

--- src/config.js
+++ src/config.js
@@ -6,20 +6,20 @@ import grants from './grants'
 
 const config = {
   firebase_config: {
-    apiKey: 'AIzaSyBQAmNJ2DbRyw8PqdmNWlePYtMP0hUcjpY',
-    authDomain: 'react-most-wanted-3b1b2.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-3b1b2.firebaseio.com',
-    projectId: 'react-most-wanted-3b1b2',
-    storageBucket: 'react-most-wanted-3b1b2.appspot.com',
-    messagingSenderId: '258373383650'
+    apiKey: 'AIzaSyDws6EBIkXKbkeRs8Tz-MB-kDcglfKeqHU',
+    authDomain: 'dvhacks-a9bf3.firebaseapp.com',
+    databaseURL: 'https://dvhacks-a9bf3.firebaseio.com',
+    projectId: 'dvhacks-a9bf3',
+    storageBucket: 'dvhacks-a9bf3.appspot.com',
+    messagingSenderId: '1080887397011'
   },
   firebase_config_dev: {
-    apiKey: 'AIzaSyB31cMH9nJnERC1WCWA7lQHnY08voLs-Z0',
-    authDomain: 'react-most-wanted-dev.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-dev.firebaseio.com',
-    projectId: 'react-most-wanted-dev',
-    storageBucket: 'react-most-wanted-dev.appspot.com',
-    messagingSenderId: '70650394824'
+    apiKey: 'AIzaSyDws6EBIkXKbkeRs8Tz-MB-kDcglfKeqHU',
+    authDomain: 'dvhacks-a9bf3.firebaseapp.com',
+    databaseURL: 'https://dvhacks-a9bf3.firebaseio.com',
+    projectId: 'dvhacks-a9bf3',
+    storageBucket: 'dvhacks-a9bf3.appspot.com',
+    messagingSenderId: '1080887397011'
   },
   firebase_providers: [
     'google.com',
